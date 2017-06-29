5de4b09067a963c630603dbde06a2f787c8dc1e7
esantellan <esantellan@sofrecom.com>
2017-06-29 17:52:34 -0300

    init project hack at home to xamarin 3.0

---
+++ HackAtHome/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HackAtHome
+{
+    [Activity(Label = "HackAtHome", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+        }
+    }
+}
+
